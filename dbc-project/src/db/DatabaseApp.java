5f4771ecda699c746d39e98c68448bd8153dbfaa
Philip Rodriguez <>
2013-03-22 14:38:38 -0400

    initial db project setup

---
+++ dbc-project/src/db/DatabaseApp.java
@@ -0,0 +1,15 @@
+package db;
+
+/**
+ *
+ * @author ptr5201
+ */
+public class DatabaseApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Application entry");
+    }
+}
